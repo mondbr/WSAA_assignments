f83bb35e72ad8e6c88c444e5dd6741034fb783b6
mondbr <mondab133@gmail.com>
2025-03-09 17:02:34 +0000

    adding comments to assignment 2

--- assignment2-carddraw.py
+++ assignment2-carddraw.py
@@ -1,5 +1,13 @@
-import requests
+# Write a program that "deals" (prints out) 5 cards
+# first you need to shuffle
+# https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+# get the deck_id, with the deck_id you can get the cards
+# https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
 
+# Author: Monika Dabrowska
+
+# import the requests library
+import requests
 
 # shuffle the deck of cards
 # get the deck id
